0ab6cd3fe8a55650f0a261c2e3ecfc6b4c928a4c
ZhuangZhuangLi <15123130007@126.com>
2014-01-21 19:49:34 +0800

    t

--- src/main/java/service/TestServiceImpl.java
+++ src/main/java/service/TestServiceImpl.java
@@ -1,6 +1,6 @@
 package service;
 import javax.jws.WebService;
-@WebService(endpointInterface = "com.digicompass.ws.service.TestService", serviceName = "Test")
+@WebService(endpointInterface = "service.TestService", serviceName = "Test")
 public class TestServiceImpl implements TestService {
 	@Override
 	public String excute(String name) {
